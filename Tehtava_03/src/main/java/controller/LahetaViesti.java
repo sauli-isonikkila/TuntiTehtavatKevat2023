98f5bad3ce4d9bca7c1418bd3a6d1e59da089639
SauliIsonikkila <sauli.isonikkila@gmail.com>
2023-04-17 08:47:18 +0300

    update 6.4.2023
    Tunnilla tehty tähän asti

---
+++ Tehtava_03/src/main/java/controller/LahetaViesti.java
@@ -0,0 +1,40 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LahetaViesti
+ */
+@WebServlet("/laheta")
+public class LahetaViesti extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LahetaViesti() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String strJSON="";
+		String viesti = "{\"viesti\": \"Tervehdys!\"}";
+		strJSON = new JSONObject().put("viesti", viesti).toString();
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println(strJSON)
+		
+	}
+
+}
